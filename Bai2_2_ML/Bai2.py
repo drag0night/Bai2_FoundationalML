ac6da57c0f9a0bbf72b2071861d2e53797f111f5
drag0night <37505407+drag0night@users.noreply.github.com>
2018-08-02 22:48:01 +0700

    abc
    ádff

---
+++ Bai2_2_ML/Bai2.py
@@ -0,0 +1,41 @@
+from sklearn.cluster import  KMeans
+import random as rd
+import numpy as np
+
+class Point:
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
+    def __hash__(self):
+        hash = 3
+        hash = 71 * hash + self.x
+        hash = 71 * hash + self.y
+        return hash
+    def __eq__(self, other):
+        return (self.x == other.x and self.y == other.y and self.__class__ == other.__class__)
+    def __arr__(self):
+        return [[self.x, self.y]]
+
+def genRamdomPoint( n ):
+    result = np.array([[0,0]])
+    sets = set()
+    i = 0
+    while i < n:
+        x = rd.randint(0, 1000)
+        y = rd.randint(0, 1000)
+        temp = Point(x, y)
+        if temp not in sets:
+            i += 1
+            result = np.concatenate((result, temp.__arr__()), axis=0)
+            sets.add(temp)
+    return np.delete(result, 0, 0)
+
+if __name__ == "__main__":
+    X = genRamdomPoint(1000)
+    print(X)
+    kmeans = KMeans(n_clusters=5, random_state= 0).fit(X)
+    fw = open("output.txt", "w")
+    for elem in X:
+        s = "-Point(%d,%d) in cluster: %d\n" % (elem[0], elem[1], kmeans.predict([elem]))
+        fw.write(s)
+    fw.close()
